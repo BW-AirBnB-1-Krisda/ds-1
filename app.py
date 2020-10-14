4fa87942c7dadc44a1a5a5473c3007d07b43d43c
Rose Wachira <r.kutchee@gmail.com>
2020-10-13 23:51:46 -0500

    correction to flask_cors import

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask,jsonify
-from flask-cors import CORS
+from flask_cors import CORS
 
 def create_app():
     '''create and configure instance of our Flask application.'''
     app = Flask(__name__)
     CORS(app)
-    app.config['CORS_HEADERS']='Content-Type'
+    #app.config['CORS_HEADERS']='Content-Type'
 
     @app.route('/', methods=['POST'])
     def root():
